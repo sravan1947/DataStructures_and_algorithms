4de8e0bb5e2a03f7dbe4bf9e7a33e6f448586c88
S.sravan kumar <unny028@gmail.com>
2021-11-08 00:07:47 +0530

    new push

---
+++ Dynamic_programming/subsequce.cpp
@@ -0,0 +1,25 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n;
+         cin>>n;
+         int a[1000];
+         for(int i=0;i<n;i++)
+         {
+            cin>>a[i];
+         }
+         int k;
+         cin>>k;
+         vector<int> v1,v2;
+         
+         for(int i=0;i<n;i++)
+         {
+             
+         }
+     }
+ }
